c705ed0f6382be8f68f431053e0a3c12e181b024
kevin170703 <correakevinfabian01@gmail.com>
2022-09-29 20:23:39 -0300

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,13 @@ import "./App.css";
 import { Route } from "react-router-dom";
 import Landing from "./components/Landing/Landing.jsx";
 import Home from "./components/Home/Home.jsx";
+import axios from "axios";
 import Create from "./components/Create/Create.jsx";
 import Detail from "./components/Detail/Detail";
+import dotenv from "dotenv";
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 function App() {
   return (
--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -2,21 +2,21 @@ import axios from "axios";
 
 export function getPokemons() {
   return async function (dispatch) {
-    const pokemons = await axios.get("http://localhost:3001/pokemons");
+    const pokemons = await axios.get("/pokemons");
     return dispatch({ type: "GET_POKEMONS", payload: pokemons.data });
   };
 }
 
 export function getTypes() {
   return async function (dispatch) {
-    const types = await axios.get("http://localhost:3001/types");
+    const types = await axios.get("/types");
     return dispatch({ type: "GET_TYPES", payload: types.data });
   };
 }
 export function getDetail(id) {
   return async function (dispatch) {
     try {
-      const detail = await axios.get(`http://localhost:3001/pokemons/${id}`);
+      const detail = await axios.get(`/pokemons/${id}`);
       return dispatch({ type: "GET_DETAIL", payload: detail.data });
     } catch (error) {
       console.log(error);
@@ -26,10 +26,7 @@ export function getDetail(id) {
 
 export function postPokemons(data) {
   return async function (dispatch) {
-    const createPokemon = await axios.post(
-      `http://localhost:3001/pokemons`,
-      data
-    );
+    const createPokemon = await axios.post(`/pokemons`, data);
     return dispatch({ type: "POST_POKEMON", payload: createPokemon });
   };
 }
@@ -37,9 +34,7 @@ export function postPokemons(data) {
 export function SearchPokemons(name) {
   return async function (dispatch) {
     try {
-      const pokemon = await axios.get(
-        `http://localhost:3001/pokemons?name=${name}`
-      );
+      const pokemon = await axios.get(`/pokemons?name=${name}`);
       return dispatch({ type: "SERACH_POKEMONS", payload: pokemon.data });
     } catch (error) {
       console.log(error);
