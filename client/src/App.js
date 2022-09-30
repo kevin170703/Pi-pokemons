b8615902f615f815d7099f1ae91de21e68fe32f9
kevin170703 <correakevinfabian01@gmail.com>
2022-09-29 21:39:10 -0300

    puerto

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Detail from "./components/Detail/Detail";
 import dotenv from "dotenv";
 dotenv.config();
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3000";
 
 function App() {
   return (
