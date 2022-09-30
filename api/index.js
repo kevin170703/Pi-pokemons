2ff925acf440fda7174ad6fb2ea7da1c47735f89
kevin170703 <correakevinfabian01@gmail.com>
2022-09-30 14:33:51 -0300

    df

--- api/index.js
+++ api/index.js
@@ -21,8 +21,8 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  var port_number = server.listen(process.env.PORT || 3001);
-  app.listen(port_number);
-  console.log("%s listening at 3001"); // eslint-disable-line no-console
+conn.sync({ force: false }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log("%s listening at 3001");
+  });
 });
